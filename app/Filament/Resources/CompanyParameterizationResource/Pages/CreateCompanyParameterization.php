2099e9cf6f493f26e7f984d14edb25e29ee97d02
Cristian Martinez Blanco <cristianjmb10@gmail.com>
2023-12-06 23:56:32 -0500

    Company Parameterization

---
+++ app/Filament/Resources/CompanyParameterizationResource/Pages/CreateCompanyParameterization.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Resources\CompanyParameterizationResource\Pages;
+
+use App\Filament\Resources\CompanyParameterizationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCompanyParameterization extends CreateRecord
+{
+    protected static string $resource = CompanyParameterizationResource::class;
+
+    protected static ?string $title = 'Crear Empresa';
+}
---
+++ app/Filament/Resources/CompanyParameterizationResource/RelationManagers/CompanyRelationManager.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources\CompanyParameterizationResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumnt;
+
+class CompanyRelationManager extends RelationManager
+{
+    protected static string $relationship = 'company';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('path_logo')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('path_logo')
+            ->columns([
+                TextColumn::make('path_logo'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
