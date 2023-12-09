72a9985afe81861f8c0930d3c1925bc5971c95fd
Cristian Martinez Blanco <cristianjmb10@gmail.com>
2023-12-09 18:22:16 -0500

    middleware structure for CRUD

---
+++ app/Http/Middleware/CRUD/CompanyParameterization/UpdateMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware\CRUD\CompanyParameterization;
+
+use Illuminate\Http\Request;
+use App\Http\Middleware\CRUD\Interfaces\ValidateData;
+use Illuminate\Support\Facades\Validator;
+
+class UpdateMiddleware implements ValidateData
+{
+    public function validate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'type_document' => 'required|in:CC,NIT,CE',
+            'identificacion' => 'required|numeric|digits_between:7,10',
+            'verification_id' => 'required|numeric|digits_between:7,10',
+            'names' => 'required|string|min:3|max:40|regex:/^[\p{L}\s]+$/u',
+            'surnames' => 'required|string|min:3|max:40|regex:/^[\p{L}\s]+$/u',
+            'business_name' => 'required|string|min:3|max:40|regex:/^[\p{L}\s]+$/u',
+            'address' => 'required|string',
+            'mobile' => 'required|numeric|digits_between:10,13',
+            'email' => 'required|email',
+            'email2' => 'email',
+            'postal_code' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()){
+            return [
+                'error' => TRUE,
+                'message' => $validator->errors()
+            ];
+        }
+
+        return ['error' => FALSE];
+    }
+}
