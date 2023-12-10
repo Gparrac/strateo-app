b8932d31be8b17e2ddd82eb408a9455191a5da29
Cristian Martinez Blanco <cristianjmb10@gmail.com>
2023-12-10 16:30:38 -0500

    office form

--- routes/custom/form.php
+++ routes/custom/form.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 // use App\Http\Middleware\CRUD\CompanyParameterizationMiddleware;
 use App\Http\Controllers\CRUD\CompanyParameterization;
+use App\Http\Controllers\CRUD\Office;
 use App\Http\Controllers\CRUD\UserParameterization;
 
 Route::group(['middleware' => 'auth:api'], function() {
     Route::match(['get', 'post', 'put', 'delete'],'/company-parameterization', CompanyParameterization::class)
         ->middleware('company_parameterization');
+    Route::match(['get', 'post', 'put', 'delete'],'/office', Office::class)
+        ->middleware('office');
 });
 Route::match(['get', 'post', 'put', 'delete'],'/user-parameterization', UserParameterization::class);
