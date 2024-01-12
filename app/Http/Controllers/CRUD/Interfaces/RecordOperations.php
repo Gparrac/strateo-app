41efd1ab56334a40d62898f191b452fc06d1ea98
Gparrac <gparrac.dev@gmail.com>
2024-01-11 23:00:04 -0500

    [update] pagination from server in users' records and append first vertion of filters

--- app/Http/Controllers/CRUD/Interfaces/RecordOperations.php
+++ app/Http/Controllers/CRUD/Interfaces/RecordOperations.php
@@ -5,5 +5,5 @@ namespace App\Http\Controllers\CRUD\Interfaces;
 interface RecordOperations
 {
     public function singleRecord($id);
-    public function allRecords($ids = null);
+    public function allRecords($ids = null, $pagination, $sorters = [], $keyword, $typeKeyword);
 }
--- app/Http/Controllers/CRUD/UserParameterizationResource/ReadResource.php
+++ app/Http/Controllers/CRUD/UserParameterizationResource/ReadResource.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CRUD\Interfaces\RecordOperations;
 use App\Models\Third;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ReadResource implements CRUD, RecordOperations
 {
@@ -15,7 +16,11 @@ class ReadResource implements CRUD, RecordOperations
         if($request->has('user_id')){
             return $this->singleRecord($request->input('user_id'));
         }else{
-            return $this->allRecords();
+            $pagination = $request->has('pagination') ? $request->input('pagination') : 10;
+            $sorters = $request->has('sorters') ? $request->input('sorters') : [];
+            $typeKeyword = $request->has('typeKeyword') ? $request->input('typeKeyword') : null;
+            $keyword = $request->has('keyword') ? $request->input('keyword') : null;
+            return $this->allRecords(null, $pagination, $sorters, $typeKeyword, $keyword );
         }
     }
 
@@ -25,11 +30,22 @@ class ReadResource implements CRUD, RecordOperations
         },'role:id,name','offices:id,name'])->find($id);
         return response()->json(['message' => 'Read: '.$id, 'data' => $data], 200);
     }
-    public function allRecords($ids = null){
+    public function allRecords($ids=null, $pagination, $sorters = [], $typeKeyword, $keyword){
         $data = User::with(['third' => function ($query){
             // $query->select('id','names','city_id'); //for specify fields and define the richment 🕐
             $query->with('city:id,name');
-        },'role:id,name','offices:id,name'])->get(); // should be with paginate() but i've still known how to consume in front ⌚
+        },'role:id,name','offices:id,name']);
+        Log::info($typeKeyword);
+        //filter query with keyword
+        if($typeKeyword && $keyword){
+            $data = $data->where($typeKeyword,$keyword);
+            // $data = $data->where($typeKeyword,);
+        }
+        //append shorters to query
+        foreach ($sorters as $key => $shorter) {
+            $data = $data->orderBy($shorter['key'], $shorter['order']);
+        }
+        $data = $data->paginate($pagination); // should be with paginate() but i've still known how to consume in front ⌚
         return response()->json(['message' => 'Read', 'data' => $data], 200);
     }
 }
